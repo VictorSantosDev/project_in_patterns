6cbdc9d265c32f5bfb21168d07000b18fadabe2a
VictorSantosDev <victoremanuel729@gmail.com>
2022-10-12 22:16:56 -0300

    Criando dashboard

--- Modules/Dashboard/Resources/views/home.blade.php
+++ Modules/Dashboard/Resources/views/home.blade.php
@@ -1,9 +1,55 @@
 @extends('dashboard::layouts.master')
 
 @section('content')
-    <h1>Chegamos</h1>
-
-    <p>
-        dashboard: {!! config('dashboard.name') !!}
-    </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Informações da carga</h3>
+                        <div class="card-tools">
+                          <span class="badge badge-primary">Label</span>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                    <div class="col-md-6">
+                        <p class="text-center">
+                            <strong>Goal Completion</strong>
+                        </p>
+                        <div class="progress-group">
+                            Total Processado
+                            <span class="float-right"><b>160</b>/200</span>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar bg-primary" style="width: 80%"></div>
+                            </div>
+                        </div>
+                        
+                        <div class="progress-group">
+                            Complete Purchase
+                            <span class="float-right"><b>310</b>/400</span>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar bg-danger" style="width: 75%"></div>
+                            </div>
+                        </div>
+                        
+                        <div class="progress-group">
+                            <span class="progress-text">Visit Premium Page</span>
+                            <span class="float-right"><b>480</b>/800</span>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar bg-success" style="width: 60%"></div>
+                            </div>
+                        </div>
+                        
+                        <div class="progress-group">
+                            Send Inquiries
+                            <span class="float-right"><b>250</b>/500</span>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar bg-warning" style="width: 50%"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- Modules/Dashboard/Resources/views/layouts/master.blade.php
+++ Modules/Dashboard/Resources/views/layouts/master.blade.php
@@ -4,16 +4,40 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Dashboard</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        <title>DashBoard</title>
 
        {{-- Laravel Mix - CSS File --}}
        {{-- <link rel="stylesheet" href="{{ mix('css/dashboard.css') }}"> --}}
 
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light mb-4" style="background-color: #a7d02f;">
+            <div class="container-fluid">
+                <a class="navbar-brand text-light border-bottom border-light" href="#">NEXTBANK</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">DashBoard</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Carteira Cliente</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
         @yield('content')
 
         {{-- Laravel Mix - JS File --}}
         {{-- <script src="{{ mix('js/dashboard.js') }}"></script> --}}
+        <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 </html>
